80a81b3443e85ca17404a87d70352886a49ba3d5
LukasOliveira <luka.oliver.1979@hotmail.com>
2021-10-24 19:20:43 -0300

    commit

--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgForm } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransferenciaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgForm
   ],
   providers: [],
   bootstrap: [AppComponent]
